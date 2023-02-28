a17d132c40dc4bf8ff64c863e55d4d795bbf8ad3
Hieu-Trung <trungthgcd17062@fpt.edu.vn>
2023-02-28 17:19:44 +0700

    test tekup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
 import logo from './logo.svg';
+import React, { useState } from 'react'
+import List1 from './image/list1.png'
+import List2 from './image/list2.png'
+import List3 from './image/list3.png'
+import List4 from './image/list4.png'
+import List5 from './image/list5.png'
+import { GrFormNext } from 'react-icons/gr'
 import './App.css';
 
 function App() {
+  const [background, setBackground] = useState(true)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <frames>
+      <div className="header">
+        <ul>
+          <li className={background ? 'style-header' : ''}>All</li>
+          <li>Manpower Supply</li>
+          <li>Mobile App / Websites</li>
+          <li>UI/UX Design</li>
+        </ul>
+      </div>
+      <div className='product'>
+        <div className="list">
+          <div className='list-product'>
+            <img src={List1} alt='list1' />
+            <div className='detail-list'>
+              <p>TOI 3D Customize E-commerce</p>
+              <GrFormNext style={{ paddingRight: '10px' }} />
+            </div>
+          </div>
+          <div className='list-product'>
+            <img src={List2} alt='list1' />
+            <div className='detail-list'>
+              <p>TOI 3D Customize E-commerce</p>
+              <GrFormNext style={{ paddingRight: '10px' }} />
+            </div>
+          </div>
+          <div className='list-product'>
+            <img src={List3} alt='list1' />
+            <div className='detail-list'>
+              <p>TOI 3D Customize E-commerce</p>
+              <GrFormNext style={{ paddingRight: '10px' }} />
+            </div>
+          </div>
+        </div>
+        <div className="list">
+          <div className='list-product'>
+            <img src={List4} alt='list1' />
+            <div className='detail-list'>
+              <p>TOI 3D Customize E-commerce</p>
+              <GrFormNext style={{ paddingRight: '10px' }} />
+            </div>
+          </div>
+          <div className='list-product'>
+            <img src={List5} alt='list1' />
+            <div className='detail-list'>
+              <p>TOI 3D Customize E-commerce</p>
+              <GrFormNext style={{ paddingRight: '10px' }} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </frames>
   );
 }
 
